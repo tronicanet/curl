d36a2e69b0480c998a988fc83e136b3f1f3df694
tronicanet <46457900+tronicanet@users.noreply.github.com>
2019-01-08 15:43:50 -0300

    Update ver_pagina_web.php

--- curl_php/ejemplos/ver_pagina_web.php
+++ curl_php/ejemplos/ver_pagina_web.php
@@ -1,15 +1,15 @@
 <?php 
  
-    // Definimos la función cURL
+
     function curl($url) {
-        $ch = curl_init($url); // Inicia sesión cURL
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); // Configura cURL para devolver el resultado como cadena
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Configura cURL para que no verifique el peer del certificado dado que nuestra URL utiliza el protocolo HTTPS
-        $info = curl_exec($ch); // Establece una sesión cURL y asigna la información a la variable $info
-        curl_close($ch); // Cierra sesión cURL
-        return $info; // Devuelve la información de la función
+        $ch = curl_init($url); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+        $info = curl_exec($ch); 
+        curl_close($ch); 
+        return $info;
     }
  
-    $sitioweb = curl("https://web.com");  // Ejecuta la función curl escrapeando el sitio web https://devcode.la and regresa el valor a la variable $sitioweb
+    $sitioweb = curl("https://web.com");
     echo $sitioweb;
 ?>
